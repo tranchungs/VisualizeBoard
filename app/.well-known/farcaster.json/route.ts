63c64b13e7403935b112d2f75674f754fbe595dd
tranchungs <41769765+tranchungs@users.noreply.github.com>
2025-06-18 02:06:56 +0700

    Update route.ts

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -4,11 +4,9 @@ import { APP_URL } from "../../../lib/constants";
 export async function GET() {
   const farcasterConfig = {
     accountAssociation: {
-      header:
-        "eyJmaWQiOjEwMjU0MzcsInR5cGUiOiJjdXN0b2R5Iiwia2V5IjoiMHgwMEE2ZkJjOTIwODg0MUIyNzJEYkE0OUE0ODdkM0E2YkVkMDVmQ2JjIn0",
+      header:"eyJmaWQiOjEwMjU0MzcsInR5cGUiOiJjdXN0b2R5Iiwia2V5IjoiMHgwMEE2ZkJjOTIwODg0MUIyNzJEYkE0OUE0ODdkM0E2YkVkMDVmQ2JjIn0",
       payload: "eyJkb21haW4iOiJ2aXN1YWxpemUtYm9hcmQudmVyY2VsLmFwcCJ9",
-      signature:
-        "MHg0MGVhMGY1MDA4MTRlYjkyY2Q5MmU2NGQ4ZjY4ZmRiOTg1YzViMDNlOWY0NWY0OTNkZjIyMTQyMzJmMDZhZTc4NzIxY2I1NzZhMDdhY2YyMjQyZTViNjg3NzU4YTZmYmIyZGMyODQzNTNhNDlkNmNjNmFlZWIyZDZlMjk2ZWZkMjFi",
+      signature:"MHg0MGVhMGY1MDA4MTRlYjkyY2Q5MmU2NGQ4ZjY4ZmRiOTg1YzViMDNlOWY0NWY0OTNkZjIyMTQyMzJmMDZhZTc4NzIxY2I1NzZhMDdhY2YyMjQyZTViNjg3NzU4YTZmYmIyZGMyODQzNTNhNDlkNmNjNmFlZWIyZDZlMjk2ZWZkMjFi",
     },
     frame: {
       version: "1",
